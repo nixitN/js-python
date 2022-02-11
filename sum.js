130e9e23e01bf7418eb029e0b647fb679f8499a8
Nikolett Toth <88892595+nixitN@users.noreply.github.com>
2022-02-11 14:13:37 +0100

    Added JS and PY files.

---
+++ sum.js
@@ -0,0 +1,8 @@
+let numericArray = [1, 3, 2, 6, 5, 3, 4, 6, 7];
+let amount = 0;
+
+for (let i=0; i<numericArray.length; i++) {
+    amount += numericArray[i];
+}
+
+alert(amount);
---
+++ sum.py
@@ -0,0 +1,3 @@
+numeric_array = [1, 3, 2, 6, 5, 3, 4, 6, 7]
+
+print(sum(numeric_array))
